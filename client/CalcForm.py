fefe818f99c02d7dc538e73cf9113071858a2ba8
Artem Ibragimov <artem.ibragimov1602@gmail.com>
2023-09-14 22:19:28 +0300

    project layout (may not build)

--- client/CalcForm.py
+++ client/CalcForm.py
@@ -1,7 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 
-
 class CalcForm(FlaskForm):
     expression = StringField("Enter the expression to calculate")
     submit = SubmitField("Calculate")
