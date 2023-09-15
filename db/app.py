e67e5356a00c22dcb2ce07be7201f2178b796ed4
lpetrov02 <lpetrov02@mail.ru>
2023-09-15 15:15:50 +0300

    Performs rebase

--- db/app.py
+++ db/app.py
@@ -1,39 +1,25 @@
-<<<<<<< HEAD
+import os
+
 from http import HTTPStatus
 from csv import Error as CSVError
-from flask import Flask, abort, request, Response
-if __name__ == "__main__":
-    from CSVWorker import CSVWorker
-else:
-    from .CSVWorker import CSVWorker
-
-app = Flask(__name__)
-app.config.from_pyfile("../config.py")
-
-csv_worker = CSVWorker("db.csv")
-csv_worker.add_first_line(["date", "question", "answer"])
-
-
-def error(code: int, message: str):
-    abort(Response(message, code))
-=======
-import os
-from enum import Enum
-
-from flask import Flask, jsonify, abort, request
+from flask import Flask, jsonify, abort, request, Response
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import func
-import os
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config.from_pyfile("../config.py")
 app.config['SQLALCHEMY_DATABASE_URI'] =\
         'sqlite:///' + os.path.join(basedir, 'database.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+
+def error(code: int, message: str):
+    abort(Response(message, code))
 
 
 class Calculation(db.Model):
@@ -71,21 +57,23 @@ class SQLWorker:
 
 sql_worker = SQLWorker()
 
-@app.route('/calculations/db', methods=['GET'])
+@app.route('/history', methods=['GET'])
 def get_tasks():
-    calculations = [[{'formula': computation.expression, 'answer': computation.result.replace('\n', '')} for computation in sql_worker.get_lines()]]
+    calculations = [[{
+        'formula': computation.expression,
+        'answer': computation.result.replace('\n', ''),
+        'datetime': str(computation.created_at),
+    } for computation in sql_worker.get_lines()]]
     return jsonify({'calculations': calculations})
->>>>>>> 7dfabe8 (Performs renaming)
 
 
 @app.route('/add', methods=['POST'])
 def create_task():
-<<<<<<< HEAD
     json = request.json
     if not json:
         error(HTTPStatus.BAD_REQUEST, message="No json provided")
     try:
-        csv_worker.add_line([json["date"], json["question"], json["answer"]])
+        sql_worker.add_line([json['question'], json['answer']])
     except KeyError as e:
         error(HTTPStatus.BAD_REQUEST, message=str(e))
     except CSVError as e:
@@ -93,30 +81,5 @@ def create_task():
     return "done", HTTPStatus.CREATED
 
 
-@app.route('/history')
-def get_tasks():
-    try:
-        calculations = [
-            {'date': line[0],
-             'question': line[1],
-             'answer': line[2].replace('\n', '')
-             } for line in csv_worker.get_lines()
-        ]
-    except CSVError as e:
-        error(HTTPStatus.INTERNAL_SERVER_ERROR, message=str(e))
-    return calculations
-
-
 if __name__ == "__main__":
     app.run(host="localhost", port=app.config["DB_PORT"], debug=True)
-=======
-    if not request.json or not 'formula' in request.json or not 'answer' in request.json:
-        abort(400)
-    
-    sql_worker.add_line([request.json['formula'], request.json['answer']])
-    return "done", 201
-
-if __name__ == '__main__':
-    app.run(port=6000, debug=True)
-    # app.run(debug=True)
->>>>>>> 7dfabe8 (Performs renaming)
